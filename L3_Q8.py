e200d420d7be15d8334be96639d00cf5c9457367
Gopika-gs <gsgopika00@gmail.com>
2022-06-11 11:35:29 +0530

    files added

---
+++ L3_Q8.py
@@ -0,0 +1,8 @@
+f = open('D:/Dora/mashup/python/read.txt','r')
+str = f .read()
+words = str.split()
+max_len = len(max(words, key = len))
+for word in words:
+    if len(word) == max_len:
+        longest_word = word
+print(longest_word)
